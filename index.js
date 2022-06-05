596515ad777c7bb306a49757d40ab9f51f950b76
Joe Scott <jbs198585@gmail.com>
2022-06-05 08:46:59 -0400

    Creating new farms

--- index.js
+++ index.js
@@ -22,13 +22,33 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
+// FARM ROUTES
+
+app.get('/farms/new', (req, res) => {
+  res.render('farms/new')
+});
+
+
+
+
+
+// PRODUCT ROUTES
+
+const categories = ['fruit', 'vegetable', 'dairy'];
+
 app.get('/products', async (req, res) => {
-  const products = await Product.find({})
-  res.render('products/index', { products })
+  const { category } = req.query;
+  if (category) {
+    const products = await Product.find({ category })
+    res.render('products/index', { products, category })
+  } else {
+    const products = await Product.find({})
+    res.render('products/index', { products, category: 'All' })
+  }
 });
 
 app.get('/products/new', (req, res) => {
-  res.render('products/new')
+  res.render('products/new', { categories })
 });
 
 app.post('/products', async (req, res) => {
