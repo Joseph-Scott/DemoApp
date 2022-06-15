4eb8230827b64ba44a725630ea245427ace82e4c
Joe Scott <jbs198585@gmail.com>
2022-06-15 07:06:59 -0400

    Finishing touches

--- index.js
+++ index.js
@@ -46,9 +46,10 @@ app.post('/farms', async (req, res) => {
   res.redirect('/farms')
 });
 
-app.get('/farms/:id/products/new', (req, res) => {
+app.get('/farms/:id/products/new', async (req, res) => {
   const { id } = req.params;
-  res.render('products/new', { categories, id })
+  const farm = await Farm.findById(id);
+  res.render('products/new', { categories, farm })
 });
 
 app.post('/farms/:id/products', async (req, res) => {
